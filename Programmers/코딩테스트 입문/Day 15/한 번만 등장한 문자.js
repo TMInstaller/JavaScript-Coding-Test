54cf55bec70a2c12c0de363be3bfc8c466c5f83c
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-07 13:56:29 +0900

    입문

---
+++ Programmers/코딩테스트 입문/Day 15/한 번만 등장한 문자.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  let ans = [];
+
+  let sArr = s.split("");
+
+  sArr.forEach((item) => {
+    if (s.indexOf(item) === s.lastIndexOf(item)) ans.push(item);
+  });
+  return ans.sort().join("");
+}
