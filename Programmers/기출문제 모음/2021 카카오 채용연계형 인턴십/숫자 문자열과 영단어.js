5af2b41061b281b8a41f7a25b435ffd552ab870c
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-27 12:09:18 +0900

    2021 카카오 채용연계형 인턴십

---
+++ Programmers/기출문제 모음/2021 카카오 채용연계형 인턴십/숫자 문자열과 영단어.js
@@ -0,0 +1,19 @@
+function solution(s) {
+  const nums = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  for (let i = 0; i < nums.length; i++) {
+    let regex = new RegExp(nums[i], "g");
+    s = s.replace(regex, i);
+  }
+  return parseInt(s);
+}
