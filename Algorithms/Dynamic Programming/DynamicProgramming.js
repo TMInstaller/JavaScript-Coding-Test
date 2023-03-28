c95fa28812ad0c030cc17ef606fff165dc63db57
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-28 14:27:45 +0900

    Dynamic Programming

---
+++ Algorithms/Dynamic Programming/DynamicProgramming.js
@@ -0,0 +1,17 @@
+// Dynamic programming is a technique for solving problems
+// by breaking them down into smaller subproblems and storing
+// the solutions to these subproblems to avoid redundant computation.
+// The Fibonacci sequence is a classic example of a problem that
+// can be solved using dynamic programming
+
+function fibonacci(n) {
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+console.log(fibonacci(10));
