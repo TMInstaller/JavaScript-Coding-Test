acfe0d4d8c002d21c44a2cce01bbb79c1bcc991b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-05-13 23:02:41 +0900

    코딩 기초 트레이닝

---
+++ Programmers/코딩 기초 트레이닝/Day 25/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,9 @@
+function solution(board, k) {
+  let answer = 0;
+  for (let i = 0; i < board.length && i <= k; i++) {
+    for (let j = 0; j < board[i].length && j <= k - i; j++) {
+      answer += board[i][j];
+    }
+  }
+  return answer;
+}
