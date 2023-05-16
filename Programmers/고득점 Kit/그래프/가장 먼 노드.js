29b513219eb5b86362a911559987a5131faed40f
TMInstaller <ajvldmlqjqclr@naver.com>
2023-05-16 10:30:07 +0900

    주석 추가

--- Programmers/고득점 Kit/그래프/가장 먼 노드.js
+++ Programmers/고득점 Kit/그래프/가장 먼 노드.js
@@ -1,24 +1,27 @@
+// edge = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
 function solution(n, edge) {
   // graph 배열을 초기화. 각 노드와 그 노드에 연결된 다른 노드들의 정보를 저장할 예정.
   const graph = Array.from({ length: n + 1 }, () => []);
-  // graph : ,,,,,,
+  // graph : [[], [], [], [], [], [], []]
 
   // distance 배열을 초기화. 각 노드로부터 1번 노드까지의 거리를 저장할 예정.
   const distance = Array(n + 1).fill(-1);
-  // distance : -1,-1,-1,-1,-1,-1,-1
+  // distance : [-1, -1, -1, -1, -1, -1, -1]
 
   // BFS에 사용될 queue. 초기에는 1번 노드만 포함.
   const queue = [1];
 
   // edge 배열에 있는 모든 간선 정보를 통해 graph를 완성.
+  // 양방향 이동이 가능하므로 다음과 같이 input한다
   edge.forEach(([a, b]) => {
     graph[a].push(b);
     graph[b].push(a);
   });
-  // graph : ,3,2,3,1,4,5,6,4,2,1,3,2,2,3
+  // graph : [[], [3, 2], [3, 1, 4, 5], [6, 4, 2, 1], [3,2], [2], [3]]
 
   // BFS 시작. 1번 노드부터 시작하므로, distance[1]을 0으로 설정.
   distance[1] = 0;
+  // distance : [-1, 0, -1, -1, -1, -1, -1]
 
   // BFS 진행.
   while (queue.length) {
